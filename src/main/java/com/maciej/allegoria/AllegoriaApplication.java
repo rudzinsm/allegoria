6cda051791a7ee778c58dd575b36bf515ccd1649
Maciej Rudziński <rudzinski.maciek@gmail.com>
2019-10-15 19:16:43 +0200

    Initial project structure from Springboot auto-generator web, redis

---
+++ src/main/java/com/maciej/allegoria/AllegoriaApplication.java
@@ -0,0 +1,13 @@
+package com.maciej.allegoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllegoriaApplication.class, args);
+	}
+
+}
