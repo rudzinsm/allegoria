ea0463d9195686c946ef0ec45666cc8ba27f3d32
Maciej Rudziński <rudzinski.maciek@gmail.com>
2019-10-15 19:59:02 +0200

    Spock library

---
+++ src/test/groovy/DummySpec.groovy
@@ -0,0 +1,17 @@
+import spock.lang.*
+
+@Unroll
+class DummySpec extends Specification {
+
+    def "minimum of #a and #b is #c"() {
+        expect:
+        Math.min(a, b) == c
+
+        where:
+        a | b || c
+        3 | 7 || 3
+        5 | 4 || 4
+        9 | 9 || 9
+    }
+
+}
